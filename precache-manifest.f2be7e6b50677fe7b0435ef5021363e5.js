8f715ea4b9dc17721ded4fddc529e19541888364
asedias <asedias@users.noreply.github.com>
2019-12-13 13:07:00 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.f2be7e6b50677fe7b0435ef5021363e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce02ddd88f0de299fe811636536ec19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07015e7c3534e1e22c23",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "90421e39e1277a37e3b6",
+    "url": "/static/css/main.a77afc81.chunk.css"
+  },
+  {
+    "revision": "07015e7c3534e1e22c23",
+    "url": "/static/js/2.35980090.chunk.js"
+  },
+  {
+    "revision": "90421e39e1277a37e3b6",
+    "url": "/static/js/main.fef53679.chunk.js"
+  },
+  {
+    "revision": "7cb8471ef4af2e2d1fc1",
+    "url": "/static/js/runtime-main.717c3d2b.js"
+  }
+]);
